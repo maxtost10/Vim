b219dbe00e2b0e453092333e1a1f1eed25ce0774
maxtost10 <m.luke.tost@gmail.com>
2025-05-12 15:33:05 +0000

    Starting Game

---
+++ Vim.py
@@ -0,0 +1,41 @@
+def level_one():
+    print("Hello")
+    print("World")
+    print("Still not there")
+    ### Nope ###
+    print("Closer ...")
+    # This is the Secret
+
+# Navigate through the labyrinth using h, j, k, l
+
+################## ########################
+################## ########################
+################## ###################    #
+################## ################### ## #
+##################                 ### ## #
+##################################     ## #
+##############            ############### #
+######## ##### ########## ###########     #
+######## ##### ########## ####        #####
+########       ##########      ############
+###########################################
+
+
+
+# Jump directly to line 20 by going 10 lines down. Type: 10 + j 
+
+
+
+
+
+
+
+
+def cursed():
+    sentence = "This code is full of badwords."
+    print(sentence)  # TODO: Clean the curse
+
+
+
+level_one()
+cursed()
